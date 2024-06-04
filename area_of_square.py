13ed657f4692d06cb69cda0df017d01c6ff9d19c
montsieur <dndott@gmail.com>
2024-06-04 21:01:26 +1000

    calculated area of square and created email form

---
+++ area_of_square.py
@@ -0,0 +1,12 @@
+#get the length from the user
+length = float(input("Enter the length of the square"))
+print(length)
+#calculate and store the area in a variable
+area =length * length
+#print the calculated area
+print(f"The calculated area of the square is {area}")
+
+# example
+# s=10
+# area_square=s*s
+# print(f"Area of the square is {area_square}")
---
+++ create_email_form.py
@@ -0,0 +1,9 @@
+#Jairo Bilbao Coder Academy -> jairo.bilbao@coderacademy.com
+first_name = input("Please, enter your first name(no blank spaces): ")
+last_name = input("Please, enter your last name(no blank spaces): ")
+company_name = input("Please, enter your company name(no blank spaces): ")
+
+print(f"{first_name}.{last_name}@{company_name}.com".lower())
+
+email = f"{first_name}.{last_name}@{company_name}.com".lower()
+print(email)

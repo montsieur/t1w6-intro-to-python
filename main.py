69e396a7601e4bc7c14ef4a3d12145bf6b2a2109
montsieur <dndott@gmail.com>
2024-06-04 20:29:29 +1000

    Added data types

--- main.py
+++ main.py
@@ -1,3 +1,63 @@
 print("Hello World!")
 print(5 - 3)
 print("Don't forget to commit!")
+
+# print("It's a comment, it won't be printed")
+
+# Data types
+# integers
+# 4
+# 7
+# print(9)
+# print("9")
+# print(type(9))
+# print (type("9"))
+
+# Floats
+# 2.5
+# 5.123123
+# print(3.1415)
+# print(type(-5.63))
+# print(type(-5))
+
+# Complex (imaginary numbers), no worries, we won't cover these kind of numbers in the course
+# 2 + 3j
+# print(type(4 + 7j))
+
+# Operators + - * / %
+# print(4 * 5)
+# print(15 / 3)
+# print(15 / 2)
+# print(7 % 2) 
+
+# String
+# "Hello world"
+# 'Hello'
+# print("This is a string")
+
+# String Concatenations, addition, join
+# print("Hello" + "world")
+# print(3 + 5)
+# print("3" + "5")
+
+# Boolean
+True
+False
+# print(type(False))
+0 # false value
+"" # false value
+# print(bool(0))
+# print(bool(""))
+# print(bool("-123"))
+# print(bool("False"))
+
+# Comparison operators <, >, <=, >=, ==, !=
+# print(2 < 3)
+# print(5 >= 5)
+# print("HELLO".lower())
+# print("Hello" == "hello")
+
+# Logical operators and, or, not
+print(True and False) # AND returns True when both operators are True
+print(False or True) # OR returns True, when one or both of the operators are True
+print(not False) # NOT returns the opposite value
